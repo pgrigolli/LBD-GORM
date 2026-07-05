a6b751c4b96492d52c1e74fdf57e81f12d3f7652
Pedro <pedrogrigolli30@gmail.com>
2026-07-05 13:31:51 -0300

    a

--- services/artista.go
+++ services/artista.go
@@ -8,6 +8,13 @@ import (
 	"gorm.io/gorm"
 )
 
+type RankingPopularidadeArtista struct {
+	Posicao             int    `gorm:"column:posicao"`
+	ArtistaID           uint   `gorm:"column:artista_id"`
+	Nome                string `gorm:"column:nome"`
+	QuantidadePlaylists int64  `gorm:"column:quantidade_playlists"`
+}
+
 func CreateArtista(artista schemas.Artista) error {
 
 	db, err := connectDB()
@@ -49,6 +56,37 @@ func GetAllArtista() ([]schemas.Artista, error) {
 
 	return artistas, err
 
+}
+
+func GetRankingPopularidadeArtista() ([]RankingPopularidadeArtista, error) {
+	db, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+
+	playlistsPorArtista := db.
+		Table(`"MUSICA" m`).
+		Select("DISTINCT m.artista_id, mp.playlist_id, mp.usuario_id").
+		Joins(`INNER JOIN "MUSICA_PLAYLIST" mp ON mp.musica_id = m.id`)
+
+	var ranking []RankingPopularidadeArtista
+	err = db.Debug().
+		Table(`"ARTISTA" a`).
+		Select("a.id AS artista_id, a.nome, COUNT(ppa.playlist_id) AS quantidade_playlists").
+		Joins("LEFT JOIN (?) ppa ON ppa.artista_id = a.id", playlistsPorArtista).
+		Group("a.id, a.nome").
+		Order("quantidade_playlists DESC").
+		Order("a.nome ASC").
+		Scan(&ranking).Error
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range ranking {
+		ranking[i].Posicao = i + 1
+	}
+
+	return ranking, nil
 }
 
 func UpdateArtista(artista schemas.Artista) (schemas.Artista, error) {
